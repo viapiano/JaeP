c502128e051d1f8cfebbb9d1c5a7c34ef1263aa4
Mario E. Viapiano <tucuarg@gmail.com>
2012-06-18 00:39:56 +0200

    übung 6.3.1

---
+++ src/Kapitel6/Uebung631/WochentagAusgabe.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Kapitel6.Uebung631;
+
+/**
+ *
+ * @author Mario
+ */
+public class WochentagAusgabe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
