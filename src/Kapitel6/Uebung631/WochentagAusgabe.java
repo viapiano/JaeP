f6c88b8c103c329b4e84b5300d4408ee16353da2
Mario Viapiano <tucuarg@gmail.com>
2012-06-18 11:42:08 +0200

    übung 6.3.1

--- src/Kapitel6/Uebung631/WochentagAusgabe.java
+++ src/Kapitel6/Uebung631/WochentagAusgabe.java
@@ -10,10 +10,17 @@ package Kapitel6.Uebung631;
  */
 public class WochentagAusgabe {
 
+    public enum WochenTag {MONTAG, DIENSTAG, MITTWOCH,
+                           DONNERSTAG, FREITAG, SAMSTAG, SONNTAG }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        for ( WochenTag wt : WochenTag.values())
+        {
+            System.out.println(wt + " ist der " + (wt.ordinal() + 1) + ". Tag der Woche");
+        }
     }
-}
+} 
